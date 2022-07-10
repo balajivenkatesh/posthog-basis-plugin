772634274d4e7be38ff6e260afa14919841f384d
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-07-10 13:55:06 -0700

    Update to use right types

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
-import { PluginEvent, PluginMeta, PluginInput } from "@posthog/plugin-scaffold";
+import {
+  PluginInput,
+  Plugin,
+  ProcessedPluginEvent,
+  Meta,
+} from "@posthog/plugin-scaffold";
+
+interface PatternsPluginInput extends PluginInput {
+  config: {
+    webhookUrl: string;
+  };
+}
 
 // Plugin method that runs on plugin load
-export async function setupPlugin({ config }: PluginMeta<PluginInput>) {
-  console.log("Setting up the Patterns plugin", config);
+export async function setupPlugin({ config }: Meta<PatternsPluginInput>) {
+  console.log("Setting up the Patterns plugin: ", config);
 }
 
 // Plugin method that processes event
-export async function onEvent(
-  event: PluginEvent,
-  meta: PluginMeta<PluginInput>
-) {
-  console.log("Sending to Patterns webhook...");
-  console.log({ event });
+export const onEvent: Plugin<PatternsPluginInput>["onEvent"] = (
+  event: ProcessedPluginEvent,
+  { config }: Meta<PatternsPluginInput>
+) => {
+  console.log("Sending to Patterns webhook...", config.webhookUrl);
+  //   console.log({ event });
   console.log("Success...");
-}
+};
