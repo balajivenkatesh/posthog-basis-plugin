106422b5e5859c7747ff5ed2e33d255ddf767540
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-07-09 17:24:18 -0700

    Initial updates

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { PluginEvent, PluginMeta, PluginInput } from "@posthog/plugin-scaffold";
+
+// Plugin method that runs on plugin load
+export async function setupPlugin({ config }: PluginMeta<PluginInput>) {
+  console.log("Setting up the Patterns plugin", config);
+}
+
+// Plugin method that processes event
+export async function processEvent(
+  event: PluginEvent,
+  meta: PluginMeta<PluginInput>
+) {
+  console.log("Processing event...");
+  console.log("Sending to Patterns webhook...");
+  console.log("Success...");
+  return event;
+}
