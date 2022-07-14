8dad3c074a924e14ec0d714d6dfd645ce7dbe679
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-07-14 11:05:30 -0700

    Handle export events function

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import {
   ProcessedPluginEvent,
   Meta,
   RetryError,
+  PluginEvent,
 } from "@posthog/plugin-scaffold";
 import fetch, { Response } from "node-fetch";
 
@@ -18,7 +19,7 @@ export async function setupPlugin({ config }: Meta<PatternsPluginInput>) {
   console.log("Loading Patterns app...");
 }
 
-// Plugin method that processes event
+// Plugin method that runs on each new event
 export const onEvent: Plugin<PatternsPluginInput>["onEvent"] = async (
   event: ProcessedPluginEvent,
   { config }: Meta<PatternsPluginInput>
@@ -30,6 +31,29 @@ export const onEvent: Plugin<PatternsPluginInput>["onEvent"] = async (
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(event),
+  });
+
+  if (response.status != 200) {
+    const data = await response.json();
+    throw new Error(
+      `Failed to send event to Patterns: ${JSON.stringify(data)}`
+    );
+  }
+  console.log("Success...");
+};
+
+// Plugin method to export events
+export const exportEvents: Plugin<PatternsPluginInput>["exportEvents"] = async (
+  events: PluginEvent[],
+  { config }: Meta<PatternsPluginInput>
+) => {
+  console.log("Exporting events to Patterns webhook...", config.webhookUrl);
+
+  let response: Response;
+  response = await fetch(config.webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(events),
   });
 
   if (response.status != 200) {
